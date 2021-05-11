54b82eb10b507fe0698893f519c1f0bdf9f39a87
Christian Sany <christian@sany.ch>
2021-05-11 22:23:32 +0200

    Update idnexjs with second days pdf files

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const fs = require('fs');
 const { mdToPdf } = require('md-to-pdf');
  
 (async () => {
-  await mdToPdf({
-    path: './00_HTML_CSS_Vorkurs/was-ist-html.md'
-  }, {
-    dest: './00_HTML_CSS_Vorkurs/dist/was-ist-html.pdf'
-  }).catch(console.error);
+  await mdToPdf(
+    { path: './00_HTML_CSS_Vorkurs/was-ist-html.md' },
+    { dest: './00_HTML_CSS_Vorkurs/dist/was-ist-html.pdf' }
+  ).catch(console.error);
 
-  await mdToPdf({
-    path: './00_HTML_CSS_Vorkurs/was-ist-css.md'
-  }, {
-    dest: './00_HTML_CSS_Vorkurs/dist/was-ist-css.pdf'
-  }).catch(console.error);
+  await mdToPdf(
+    { path: './00_HTML_CSS_Vorkurs/was-ist-css.md' },
+    { dest: './00_HTML_CSS_Vorkurs/dist/was-ist-css.pdf' }
+  ).catch(console.error);
+
+  await mdToPdf(
+    { path: './01_HTML_CSS_Basics/fortgeschrittene-basics-css.md' },
+    { dest: './01_HTML_CSS_Basics/dist/fortgeschrittene-basics-css.pdf' }
+  ).catch(console.error);
+
+  await mdToPdf(
+    { path: './01_HTML_CSS_Basics/fortgeschrittene-basics-html.md' },
+    { dest: './01_HTML_CSS_Basics/dist/fortgeschrittene-basics-html.pdf' }
+  ).catch(console.error);
 })();
